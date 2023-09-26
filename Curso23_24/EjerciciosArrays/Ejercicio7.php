bf53b69bd7444ca2da5c30ecd22efae7af2ee559
Fer <fernandomartinezortiz003@gmail.com>
2023-09-26 13:08:40 +0200

    1-8

--- Curso23_24/EjerciciosArrays/Ejercicio7.php
+++ Curso23_24/EjerciciosArrays/Ejercicio7.php
@@ -3,23 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio6</title>
+    <title>Ejercicio7</title>
 </head>
 <body>
+
     <?php
 
-        $arr[]='Madrid';
-        $arr[]='Barcelona';
-        $arr[]='Londres';
-        $arr[]='New York';
-        $arr[]='Los Angeles';
-        $arr[]='Chicago';
+        $arr['MD']='Madrid';
+        $arr['BC']='Barcelona';
+        $arr['LD']='Londres';
+        $arr['NY']='New York';
+        $arr['LA']='Los Angeles';
+        $arr['CG']='Chicago';
 
-        
+
         foreach ($arr as $indice => $valores) {
             echo "<p>La ciudad con el indice ".$indice." tiene el nombre de: ".$valores."</p>";
         }
 
     ?>
+    
 </body>
 </html>
