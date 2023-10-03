bd8afcfbba4011d3fc5aadf642837725b7161305
Fer <fernandomartinezortiz003@gmail.com>
2023-10-03 09:42:37 +0200

    4 corregido por el profe

--- Curso23_24/TeoriaStrings/Ejercicios/Ejercicio4.php
+++ Curso23_24/TeoriaStrings/Ejercicios/Ejercicio4.php
@@ -10,11 +10,73 @@
 
         <?php
 
+            const VALOR = array("M" => 1000,"D" => 500,"C" => 100,"L" => 50,"X" => 10,"V" => 5,"I" => 10);           
+
+            function letras_bien($texto){
+                $bien = true;
+
+                for ($i=0; $i < strlen($texto); $i++) { 
+                    if(!isset(VALOR[$texto[$i]])){
+                        $bien=false;
+                        break;
+                    }
+                }
+                return $bien;
+            }
+
+            function orden_bueno($texto){
+
+                $bien = true;
+
+                for ($i=0; $i < strlen($texto)-1; $i++) { 
+                    if(VALOR[$texto[$i]]<VALOR[$texto[$i+1]]){
+                        $bien=false;
+                        break;
+                    }
+                }
+                return $bien;
+
+            }
+
+            function repite_bien($texto){
+
+                $veces["I"]=4;
+                $veces["V"]=1;
+                $veces["X"]=4;
+                $veces["L"]=1;
+                $veces["C"]=4;
+                $veces["D"]=1;
+                $veces["M"]=4;
+
+                $bien = true;
+
+                for ($i=0; $i < strlen($texto); $i++) { 
+
+                    $veces[$texto[$i]]--;
+                    if($veces[$texto[$i]]==-1){
+                        $bien=false;
+                        break;
+                    }
+                }
+
+                return $bien;
+
+            }
+
+
+            function es_correcto_romano($texto){
+
+                return letras_bien($texto) && orden_bueno($texto) && repite_bien($texto);
+
+            }
+
             //Si los campos estan vacios o no contienen la longitud adecuada
             if (isset($_POST["comparar"])) {
 
-                
-                $errorPrimera = $_POST["primera"] == "" || is_numeric($_POST["primera"]);
+
+                $texto=trim($_POST['primera']);
+                $texto_m=strtoupper($texto);
+                $errorPrimera = $_POST["primera"] == "" || !es_correcto_romano($texto_m);
 
 
                 $errorFormu = $errorPrimera;
@@ -25,7 +87,7 @@
         ?>
 
 
-    <form action="Ejercicio3.php" method="post" enctype="multipart/form-data">
+    <form action="Ejercicio4.php" method="post" enctype="multipart/form-data">
 
         <div style="background-color:lightblue; border:solid; padding:5px;">
 
@@ -36,8 +98,14 @@
                 <label for="primera">Numero:</label>
                 <input type="text" name="primera" id="primera" value="<?php if(isset($_POST['primera'])) echo trim($_POST['primera'])?>"/>
                 <?php
-                    if (isset($_POST["comparar"]) && $errorPrimera) {
-                        echo "<span class='error'>*Introduce una frase*</span>";
+                    if (isset($_POST["comparar"]) && $errorFormu) {
+
+                        if($_POST["primera"]==''){
+                            echo "<span class='error'>*Campo vacio*</span>";
+                        }else {
+                            echo "<span class='error'>*No has escrito un numero romano correcto*</span>";
+                        }
+                        
                     }
                 ?>
             </p>
@@ -53,11 +121,16 @@
 
             if (isset($_POST["comparar"]) && !$errorFormu) {
 
+                $res=0;
+                for ($i=0; $i < strlen($texto_m) ; $i++) { 
+                    $res+=VALOR[$texto_m[$i]];
+                }
+
                 echo'<div style="background-color:lightgreen; border:solid; margin-top:10px; padding:5px;">';
 
                     echo'<h1 style="text-align:center">Romanos a arabes - Resultado</h1>';
 
-                    echo '<p> El numero '.$_POST['primera'].' se escribe en cifras arabes</p>';
+                    echo '<p> El numero '.$texto_m.' se escribe en cifras arabes: '.$res.'</p>';
 
 
                 echo'</div>';
--- Curso23_24/TeoriaStrings/Ejercicios/Ejercicio5.php
+++ Curso23_24/TeoriaStrings/Ejercicios/Ejercicio5.php
@@ -3,18 +3,80 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio5</title>
+    <title>Ejercicio4</title>
     <style>.error{color:red;}</style>
 </head>
 <body>
 
         <?php
 
+            const VALOR = array("M" => 1000,"D" => 500,"C" => 100,"L" => 50,"X" => 10,"V" => 5,"I" => 10);           
+
+            function letras_bien($texto){
+                $bien = true;
+
+                for ($i=0; $i < strlen($texto); $i++) { 
+                    if(!isset(VALOR[$texto[$i]])){
+                        $bien=false;
+                        break;
+                    }
+                }
+                return $bien;
+            }
+
+            function orden_bueno($texto){
+
+                $bien = true;
+
+                for ($i=0; $i < strlen($texto)-1; $i++) { 
+                    if(VALOR[$texto[$i]]<VALOR[$texto[$i+1]]){
+                        $bien=false;
+                        break;
+                    }
+                }
+                return $bien;
+
+            }
+
+            function repite_bien($texto){
+
+                $veces["I"]=4;
+                $veces["V"]=1;
+                $veces["X"]=4;
+                $veces["L"]=1;
+                $veces["C"]=4;
+                $veces["D"]=1;
+                $veces["M"]=4;
+
+                $bien = true;
+
+                for ($i=0; $i < strlen($texto); $i++) { 
+
+                    $veces[$texto[$i]]--;
+                    if($veces[$texto[$i]]==-1){
+                        $bien=false;
+                        break;
+                    }
+                }
+
+                return $bien;
+
+            }
+
+
+            function es_correcto_romano($texto){
+
+                if(!is_numeric($texto) || $texto < 0)
+
+                
+            }
+
             //Si los campos estan vacios o no contienen la longitud adecuada
             if (isset($_POST["comparar"])) {
 
-                
-                $errorPrimera = $_POST["primera"] == "" || is_numeric($_POST["primera"]);
+
+                $texto=trim($_POST['primera']);
+                $errorPrimera = $_POST["primera"] == "" || !es_correcto_romano($texto);
 
 
                 $errorFormu = $errorPrimera;
@@ -25,19 +87,25 @@
         ?>
 
 
-    <form action="Ejercicio3.php" method="post" enctype="multipart/form-data">
+    <form action="Ejercicio4.php" method="post" enctype="multipart/form-data">
 
         <div style="background-color:lightblue; border:solid; padding:5px;">
 
             <h1 style="text-align:center">Arabes a romanos - Formulario</h1>
 
-            <p>Dime un numero en numeros arabes y lo convertire a numeros romanos</p>
+            <p>Dime un numero en numeros arabes y lo convertire en Romanos</p>
             <p>
                 <label for="primera">Numero:</label>
                 <input type="text" name="primera" id="primera" value="<?php if(isset($_POST['primera'])) echo trim($_POST['primera'])?>"/>
                 <?php
-                    if (isset($_POST["comparar"]) && $errorPrimera) {
-                        echo "<span class='error'>*Introduce una frase*</span>";
+                    if (isset($_POST["comparar"]) && $errorFormu) {
+
+                        if($_POST["primera"]==''){
+                            echo "<span class='error'>*Campo vacio*</span>";
+                        }else {
+                            echo "<span class='error'>*No has escrito un numero romano correcto*</span>";
+                        }
+                        
                     }
                 ?>
             </p>
@@ -53,12 +121,17 @@
 
             if (isset($_POST["comparar"]) && !$errorFormu) {
 
+                $res=0;
+                for ($i=0; $i < strlen($texto_m) ; $i++) { 
+                    $res+=VALOR[$texto_m[$i]];
+                }
+
                 echo'<div style="background-color:lightgreen; border:solid; margin-top:10px; padding:5px;">';
 
-                    echo'<h1 style="text-align:center">Arabes a romanos - Resultado</h1>';
+                    echo'<h1 style="text-align:center">Arabes a romanos  - Resultado</h1>';
 
-                    echo '<p> El numero '.$_POST['primera'].' se escribe en numeros en romanos</p>';
-                    
+                    echo '<p> El numero '.$texto_m.' se escribe en cifras arabes: '.$res.'</p>';
+
 
                 echo'</div>';
                 
