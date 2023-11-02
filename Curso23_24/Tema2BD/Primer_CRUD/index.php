9dc1eb37725bf1d5be1650fbd2238bcbf7593a68
Fer <fernandomartinezortiz003@gmail.com>
2023-11-02 14:43:22 +0100

    CRUD

---
+++ Curso23_24/Tema2BD/Primer_CRUD/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=ç, initial-scale=1.0">
+    <title>Practica 1º CRUD</title>
+    <style>
+        table,th,td {
+            border:1px solid black;
+        }
+        table{
+            border-collapse: collapse; 
+            width: 30%; 
+            text-align: center;
+        }
+        th{
+            background-color: #CCC;
+        }
+        table img{
+            width: 50px;
+            height: 50px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Listado de los usuarios</h1>
+    <?php
+
+        try{
+            
+            $conexion=mysqli_connect("localhost","jose","josefa","bd_foro");
+            mysqli_set_charset($conexion,"utf8");
+
+        }catch(Exception $e){
+
+            die("<p>No ha podido conectarse a la base de datos: ".$e->getMessage()."</p></body></html>");
+        }
+
+        $consulta="select * from usuarios";
+
+
+        try {
+
+            $resultado=mysqli_query($conexion,$consulta);
+            
+        } catch (Exception $e) {
+    
+            mysqli_close($conexion);
+            die("<p>Imposible realizar la consulta: ".$e->getMessage()."</p></body></html>");
+            
+        }
+        
+
+        //if(mysqli_num_rows($resultado) > 1){}     Condiciona si la tabla esta vacia o no
+        
+        echo "<table>";
+
+        echo "<tr><th>Nombre de usuario</th><th>Borrar</th><th>Editar</th></tr>";
+        while ($tupla=mysqli_fetch_assoc($resultado)) {
+            
+            echo "<tr>";
+            echo "<td>".$tupla["nombre"]."</td>";
+            echo "<td><img src='images/x.png' alt='Imagen de Borrar' title='Imagen de Borrar'</td>";
+            echo "<td><img src='images/lapiz.png' alt='Imagen de Editar' title='Imagen de Editar'</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+
+        echo "<form action='usuario_nuevo.php' method='post'>";
+        echo "<p><button type='submit' name='btnNuevoUsuario'>Insertar nuevo usuario</button></p>";
+        echo "</form>";
+
+        mysqli_close($conexion);
+    ?>
+    
+</body>
+</html>
---
+++ Curso23_24/Tema2BD/Primer_CRUD/usuario_nuevo.php
@@ -0,0 +1,59 @@
+<?php
+
+    if(isset($_POST["btnNuevoUsuario"]) || isset($_POST["continuar"])){
+
+        if(isset($_POST["continuar"])){
+            $error_nombre=$_POST["nombre"]=="";
+            $error_usuario=$_POST["usuario"]=="";
+            $error_clave=$_POST["clave"]=="";
+            $error_email=$_POST["email"]== "" || !filter_var($_POST["email"],FILTER_VALIDATE_EMAIL);
+            $error_form= $error_nombre || $error_usuario ||  $error_clave || $error_email; 
+        }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Nuevo Usuario</h1>
+    <form action="usuario_nuevo.php" method="post" enctype="multipart/form-data">
+        <p>
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre" id="nombre" maxlength="30" value="">
+        </p>
+
+        <p>
+            <label for="usuario">Usuario:</label>
+            <input type="text" name="usuario" id="usuario" maxlength="20" value="">
+        </p>
+
+        <p>
+            <label for="clave">Contraseña:</label>
+            <input type="password" name="clave" maxlength="15" id="clave">
+        </p>
+
+        <p>
+            <label for="email">Email:</label>
+            <input type="text" name="email" id="email" maxlength="50" value="">
+        </p>
+
+        <p>
+            <button type="submit" name="continuar">Continuar</button>
+            <button type="submit" name="volver">Volver</button>
+        </p>
+    </form>
+</body>
+</html>
+<?php
+
+}else{
+    header("Location:index.php");
+    exit;
+}
+?>
